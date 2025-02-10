17f64615c7e0ba1aef8b43d9b63b16ef443195eb
EA - Thiago Modesto Santos <thiago_santos@educadventista.org>
2025-02-10 16:32:01 -0300

    atualizacao

---
+++ src/components/PrimeiroComponente.jsx
@@ -0,0 +1,10 @@
+function PrimeiroComponente () {
+    return (
+        <div>
+            <h1>Sou o Primeiro Título</h1>
+            <p>Sou o Primeiro parágrafo</p>
+        </div>
+    )
+}
+
+export default PrimeiroComponente
---
+++ src/components/SegundoComponente.jsx
@@ -0,0 +1,10 @@
+function SegundoComponente () {
+    return (
+        <div>
+            <h1>Sou o segundo título</h1>
+            <p>Sou o segundo parágrafo</p>
+        </div>
+    )
+}
+
+export default SegundoComponente
